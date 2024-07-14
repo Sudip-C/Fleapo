93e07b83573b0859c8d94bc7e9a338aa8e0175ed
Sudip Chowdhury <rexalex826@gmail.com>
2024-07-14 21:25:57 +0530

    responsiveness remain

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navItems = document.querySelectorAll('.nav-item');
 
-    navItems.forEach(item => {
-        item.addEventListener('click', function() {
-            navItems.forEach(nav => nav.classList.remove('active'));
-            this.classList.add('active');
-            const imageUrl = this.getAttribute('data-image');
-            this.style.setProperty('--underline-image', `url(${imageUrl})`);
-        });
-    });
-});
